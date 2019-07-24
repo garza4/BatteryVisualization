454da2e2eef99a867c142b4c4da323488a359e28
Bobby <garza4@southwestern.edu>
2019-07-24 10:29:07 -0500

    setting up xml files and creating graph functions

--- app/src/main/java/com/example/batteryvisualization/MainActivity.java
+++ app/src/main/java/com/example/batteryvisualization/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.batteryvisualization;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
-
+import com.github.mikephil.charting.charts.BarChart;
+import android.appwidget.AppWidgetHost;
+import android.os.BatteryManager;
 public class MainActivity extends AppCompatActivity {
+    //https://github.com/PhilJay/MPAndroidChart/blob/master/MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/BarChartActivity.java
+    private BarChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chart = findViewById(R.id.barchart);
+        float batteryLvl = getData();
     }
+
+    public float getData(){
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = this.registerReceiver(null, ifilter);
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level / (float)scale;
+        return batteryPct;
+    }
+
+//    public BarChart graph(){
+//
+//
+//    }
 }
